8a3ccfef8e2fcbc6a02a55820291078f95ddaa93
moniekhoncoop <moniekhoncoop@hotmail.com>
2019-08-28 11:10:28 +0200

    Share all the files

--- src/main/kotlin/furhatos/app/demoleeuwarden/flow/general.kt
+++ src/main/kotlin/furhatos/app/demoleeuwarden/flow/general.kt
@@ -11,7 +11,7 @@ val Idle: State = state {
         furhat.setTexture("Geremy") // Set mask to Geremy (Male with beard)
         if (users.count > 0) {
             furhat.attend(users.random)
-            goto(Start)
+            goto(Aanspreek)
         }
     }
 
@@ -21,11 +21,11 @@ val Idle: State = state {
 
     onUserEnter {
         furhat.attend(it)
-        goto(Start)
+        goto(Aanspreek)
     }
 }
 
-val Interaction: State = state {
+val TerugNaarState: State = state {
 
     var nomatches = 0 // create variable nomatches and start with 0
     var silences = 0 // create variable silences and start with 0
@@ -34,7 +34,7 @@ val Interaction: State = state {
         if (users.count > 0) {
             if (it == users.current) {
                 furhat.attend(users.other)
-                goto(Start)
+                goto(Aanspreek)
             } else {
                 furhat.glance(it)
             }
@@ -74,6 +74,81 @@ val Interaction: State = state {
 
     onResponseFailed {
         furhat.say("Sorry ik moet even rusten. Stel je vragen maar aan mijn menselijke collega's.")
+
+        exitProcess(0)
+    }
+
+}
+
+val IkWeetNiet: State = state {
+
+    var nomatches = 0 // create variable nomatches and start with 0
+    var silences = 0 // create variable silences and start with 0
+
+    onUserLeave(instant = true) {
+        if (users.count > 0) {
+            if (it == users.current) {
+                furhat.attend(users.other)
+                goto(Aanspreek)
+            } else {
+                furhat.glance(it)
+            }
+        } else {
+            goto(Idle)
+        }
+    }
+
+    onUserEnter(instant = true) {
+        furhat.glance(it)
+    }
+
+    // Create in the Interaction state the onResponse, onNoResponse and onResponseFailed
+
+    onResponse {
+        nomatches++ // increment variable nomatches
+
+        when(nomatches) {
+            // if there are no matches do the following
+            1 -> {
+                furhat.say {
+                    random {
+                        + "Deze vraag begrijp ik niet, kan je een andere vraag stellen?"
+                        + "Wat zeg jij nou? Vraag eens wat anders astublieft"
+                        + "Dat vraag je toch niet op de eerste date, stel maar een beschaafde vraag"
+                        + "Sorry ik spreek alleen fries, je kan vragen naar m'n naam of mijn favoriete film"
+                    }
+                }
+                reentry()
+            }
+        }
+    }
+
+    onNoResponse {
+        silences++ // increment silences
+
+        when(silences) {
+            // if silences is true do the following
+
+            1 -> {
+                furhat.say {
+                random {
+                    + "Als je het even niet weet, hiernaast staat een lijstje met vragen die je mij kan stellen"
+                    + "Niet zo zenuwachtig, je kan mij gerust wat vragen "
+                    + "Ik bijt niet hoor, vraag maar raak"
+                    + "Heb je het lijstje al gezien met vragen die je mij kan stellen"
+
+                }
+            }
+                reentry()
+
+            }
+        }
+    }
+
+    onResponseFailed {
+        furhat.setVoice(Language.ENGLISH_GB, Gender.MALE) // setVoice to Dutch and Male voice
+        furhat.setTexture("Ivan") // Set mask to Ivan (Alien face)
+        furhat.say("Error Error, Self destruction start in 10, 9, 8, 7, 6, ... Just kidding")
 
         exitProcess(0)
     }
